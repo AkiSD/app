29519f539e8417a12f124b7ade499d87e7cc74ba
AkiSD <ololoev9625@gmail.com>
2020-05-14 08:19:52 +0500

    1234123
    1234

--- js/script.js
+++ js/script.js
@@ -5,4 +5,26 @@ $(document).ready(function () {
         prevArrow: '<button class="about-slider__prev"><i class="fa fa-angle-left"></i></button>',
         nextArrow: '<button class="about-slider__next"><i class="fa fa-angle-right"></i></button>',
     });
+
+
+    $('.works-btns__btn').click(function () {
+        $('.works-btns__btn').removeClass('works-btns__btn_active');
+        $(this).addClass('works-btns__btn_active');
+        filterPhoto('.' + $(this).attr('data-filter'));
+    })
+
+
+    function filterPhoto(photo) {
+        $('.works .col-xl-4').filter(photo).show();
+        $('.works .col-xl-4').not(photo).hide();
+    }
+
+
+    $('.clients-slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: 'clients-slider__dots'
+    })
+
 })
